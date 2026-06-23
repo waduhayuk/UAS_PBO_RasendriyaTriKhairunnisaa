45a7feb074a9f63041bf0c044c4b7cb86a658977
waduhayuk <rasenputar@gmail.com>
2026-06-23 10:58:40 +0700

    revisi koneksi

--- koneksi/database.php
+++ koneksi/database.php
@@ -1,14 +1,27 @@
 <?php
 
-$koneksi = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "db_uas_pbo_trpl1b_rasendriyatrikhairunnisaa"
-);
+class Database 
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "db_uas_pbo_trpl1b_rasendriyatrikhairunnisaa";
+    public $koneksi;
 
-if (!$koneksi) {
-    die("Koneksi gagal : " . mysqli_connect_error());
+    public function __construct() 
+    {
+        // Membuat koneksi menggunakan objek MySQLi (OOP Murni)
+        $this->koneksi = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
+        // Mengecek apakah koneksi eror lewat properti objek
+        if ($this->koneksi->connect_error) {
+            die("Koneksi gagal: " . $this->koneksi->connect_error);
+        }
+    }
 }
+
+// Membuat objek dari class Database agar bisa dipakai di index.php
+$db = new Database();
+$koneksi = $db->koneksi;
 
 ?>
